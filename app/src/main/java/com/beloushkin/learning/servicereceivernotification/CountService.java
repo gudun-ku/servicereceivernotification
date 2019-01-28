2cac9be54068dfbcfb2c5987477424bcf9223c1f
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-01-28 10:52:47 +0300

    Foreground plus notification

--- app/src/main/java/com/beloushkin/learning/servicereceivernotification/CountService.java
+++ app/src/main/java/com/beloushkin/learning/servicereceivernotification/CountService.java
@@ -1,8 +1,13 @@
 package com.beloushkin.learning.servicereceivernotification;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import java.util.concurrent.Executors;
@@ -14,6 +19,8 @@ public class CountService extends Service {
     public static final String TAG = CountService.class.getSimpleName();
     public static final String TIME = "TIME";
     private ScheduledExecutorService mScheduledExecutorService;
+    private NotificationManager mNotificationManager;
+    private NotificationCompat.Builder mNotificationBuilder;
 
     public CountService() {
     }
@@ -27,18 +34,29 @@ public class CountService extends Service {
     @Override
     public void onCreate() {
         Log.d(TAG, "onCreate: ");
-       mScheduledExecutorService = Executors.newScheduledThreadPool(1);
+        mScheduledExecutorService = Executors.newScheduledThreadPool(1);
+
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotificationBuilder = getNotificationBuilder();
+
+        mNotificationBuilder.setContentTitle("Count service notification")
+                .setSmallIcon(R.drawable.ic_launcher_foreground);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(TAG, "onStartCommand: ");
 
+        startForeground(123,getNotification("Start service"));
+
         mScheduledExecutorService.scheduleAtFixedRate(new Runnable() {
             @Override
             public void run() {
                 long currTime = System.currentTimeMillis();
                 Log.d(TAG, "run: " + currTime);
+
+
+                mNotificationManager.notify(123,getNotification(" time is " + currTime));
                 Intent simpleIntent = new Intent(SimpleReceiver.SIMPLE_ACTION);
                 simpleIntent.putExtra(TIME, currTime);
                 sendBroadcast(simpleIntent);
@@ -49,10 +67,33 @@ public class CountService extends Service {
         return START_STICKY;
     }
 
+    private Notification getNotification( String text) {
+        return mNotificationBuilder.setContentText(text)
+                .build();
+    }
+
     @Override
     public void onDestroy() {
         //!!! stop the executor service!!!
         mScheduledExecutorService.shutdownNow();
         Log.d(TAG, "onDestroy: ");
     }
+
+
+    private NotificationCompat.Builder getNotificationBuilder() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return new NotificationCompat.Builder(this);
+        } else {
+            String channelId = getString(R.string.cs_channel_id);
+            if(mNotificationManager.getNotificationChannel(channelId) == null) {
+                NotificationChannel channel =
+                        new NotificationChannel(channelId,
+                                "Count service", NotificationManager.IMPORTANCE_LOW);
+            }
+
+            return  new NotificationCompat.Builder(this, getString(R.string.cs_channel_id));
+        }
+    }
+
+
 }
--- app/src/main/java/com/beloushkin/learning/servicereceivernotification/MainActivity.java
+++ app/src/main/java/com/beloushkin/learning/servicereceivernotification/MainActivity.java
@@ -2,12 +2,12 @@ package com.beloushkin.learning.servicereceivernotification;
 
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
